318cc7dd95c345dd75c511ad08f890ddadcf09a9
DussaUday <dussauday469@gmail.com>
2025-04-07 13:35:58 +0530

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";     
-$username = "root";       
-$password = "";           
-$database = "uday";       
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";     
+$username = "2chHuJJjXN4T7Z9.root";       
+$password = "Mcg8yMpNokw8nhve";           
+$database = "test";       
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
